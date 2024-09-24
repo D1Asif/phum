c0d4c7c4cf1009726a842a1612beeaac31d81faa
Dewan Asifur Rahman <dasifrahman@gmail.com>
2024-09-24 14:27:24 +0600

    refresh token implemented

---
+++ src/pages/admin/academicManagement/AcademicSemester.tsx
@@ -0,0 +1,12 @@
+import { useGetAllSemestersQuery } from "../../../redux/features/academicSemester/academicSemesterApi"
+
+
+export default function AcademicSemester() {
+    const { data } = useGetAllSemestersQuery(undefined);
+
+    console.log(data);
+
+    return (
+        <div>AcademicSemester</div>
+    )
+}
